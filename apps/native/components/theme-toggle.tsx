0507dd58eb7b1aa6826e6c45e209f447253f381d
Felix Berger <felix.enok.berger@gmail.com>
2026-01-17 23:40:12 +0000

    Respect reduced motion setting in ThemeToggle animations

--- apps/native/components/theme-toggle.tsx
+++ apps/native/components/theme-toggle.tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
 import * as Haptics from "expo-haptics";
 import { Platform, Pressable } from "react-native";
-import Animated, { FadeOut, ZoomIn } from "react-native-reanimated";
+import Animated, { FadeOut, ZoomIn, useReducedMotion } from "react-native-reanimated";
 import { withUniwind } from "uniwind";
 
 import { useAppTheme } from "@/contexts/app-theme-context";
@@ -10,6 +10,7 @@ const StyledIonicons = withUniwind(Ionicons);
 
 export function ThemeToggle() {
   const { toggleTheme, isLight } = useAppTheme();
+  const reduceMotion = useReducedMotion();
 
   return (
     <Pressable
@@ -24,11 +25,19 @@ export function ThemeToggle() {
       accessibilityRole="button"
     >
       {isLight ? (
-        <Animated.View key="moon" entering={ZoomIn} exiting={FadeOut}>
+        <Animated.View 
+          key="moon" 
+          entering={reduceMotion ? undefined : ZoomIn} 
+          exiting={reduceMotion ? undefined : FadeOut}
+        >
           <StyledIonicons name="moon" size={20} className="text-foreground" />
         </Animated.View>
       ) : (
-        <Animated.View key="sun" entering={ZoomIn} exiting={FadeOut}>
+        <Animated.View 
+          key="sun" 
+          entering={reduceMotion ? undefined : ZoomIn} 
+          exiting={reduceMotion ? undefined : FadeOut}
+        >
           <StyledIonicons name="sunny" size={20} className="text-foreground" />
         </Animated.View>
       )}
